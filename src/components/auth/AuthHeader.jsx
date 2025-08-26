4980c41d22dd609f4e413f9ee3ed76cade00c406
Ilmanmdifa <ilmanmdifa63@gmail.com>
2025-08-26 14:23:58 +0700

    fix(auth): change logo to svg, edit shadow color in border and edit size logo in AuthHeader

--- src/components/auth/AuthHeader.jsx
+++ src/components/auth/AuthHeader.jsx
@@ -1,9 +1,9 @@
-import logo from "../../assets/images/Logo.png";
+import logo from "../../assets/images/Logo.svg";
 
 const AuthHeader = () => {
   return (
-    <header className="flex space-between items-center p-6 md:px-28 md:py-6 border-solid border-1 border-[#F1F1F1] shadow-md md:shadow-none">
-      <img src={logo} alt="logo" />
+    <header className="flex space-between items-center p-6 md:px-28 md:py-6 border-solid border-1 border-[#F1F1F1] shadow-md shadow-shadowHeader md:shadow-none">
+      <img src={logo} alt="logo" className="h-6 md:h-8" />
     </header>
   );
 };
