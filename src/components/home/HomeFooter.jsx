280667128d249fa64e5a89ea7fa538bcc35d2cee
Ilmanmdifa <ilmanmdifa63@gmail.com>
2025-08-28 22:36:18 +0700

    fix(home): correct typo in HomeFooter on importing logo

--- src/components/home/HomeFooter.jsx
+++ src/components/home/HomeFooter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../assets/images/logo.svg";
+import logo from "../../assets/images/Logo.svg";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
