ba38120c086e2a88be9edd73608c1a003e34b5ae
Ilmanmdifa <ilmanmdifa63@gmail.com>
2025-08-28 11:33:41 +0700

    refactor(home): create component for HomeHero and apply css

---
+++ src/components/home/HomeHero.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const HomeHero = () => {
+  return (
+    <section className="flex items-center justify-center bg-hero w-[320px] h-[400px] md:w-[1200px] rounded-[10px] shadow-md mx-auto">
+      <div className="relative flex flex-col text-center gap-3 items-center py-[36px] md:py-[64px] px-[20px] md:px-[140px] font-[DM_sans] text-[#ffffff] z-1">
+        <h1 className="text-2xl md:text-5xl font-bold leading-[110%] font-[Poppins]">
+          Revolusi Pembelajaran: Temukan Ilmu Baru melalui Platform Video
+          Interaktif!
+        </h1>
+        <p className="text-sm md:text-base font-medium leading-[140%] tracking-[0.2px]">
+          Temukan ilmu baru yang menarik dan mendalam melalui koleksi video
+          pembelajaran berkualitas tinggi. Tidak hanya itu, Anda juga dapat
+          berpartisipasi dalam latihan interaktif yang akan meningkatkan
+          pemahaman Anda.
+        </p>
+        <button className="text-sm md:text-base h-10 md:h-10.5 w-auto mt-3 rounded-[10px] p-2.5 md:py-[10px] md:px-[26px] bg-[#3ECF4C] hover:bg-[#39b946] cursor-pointer">
+          Temukan Video Course untuk Dipelajari!
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default HomeHero;
--- src/components/home/HomeMain.jsx
+++ src/components/home/HomeMain.jsx
@@ -1,29 +1,16 @@
 import React from "react";
+import HomeHero from "./HomeHero";
+import HomeContent from "./HomeContent";
 
 const HomeMain = () => {
   return (
-    <main className="h-full flex items-center md:px-[120px] md:py-[64px] gap-6 flex-col px-[20px] py-[28px] bg-[#fff7d7cc]">
-      <section className="bg-hero w-[320px] h-[400px] md:w-[1200px]  rounded-[10px] shadow-md ">
-        <div className="relative flex flex-col text-center gap-3 items-center py-[36px] md:py-[64px] px-[20px] md:px-[140px] font-[DM_sans] text-[#ffffff] z-1">
-          <h1 className="text-2xl md:text-5xl font-bold leading-[110%] font-[Poppins]">
-            Revolusi Pembelajaran: Temukan Ilmu Baru melalui Platform Video
-            Interaktif!
-          </h1>
-          <p className="text-sm md:text-base font-medium leading-[140%] tracking-[0.2px]">
-            Temukan ilmu baru yang menarik dan mendalam melalui koleksi video
-            pembelajaran berkualitas tinggi. Tidak hanya itu, Anda juga dapat
-            berpartisipasi dalam latihan interaktif yang akan meningkatkan
-            pemahaman Anda.
-          </p>
-          <button className="text-sm md:text-base h-10 md:h-10.5 w-auto mt-3 rounded-[10px] p-2.5 md:py-[10px] md:px-[26px] bg-[#3ECF4C] hover:bg-[#39b946] cursor-pointer">
-            Temukan Video Course untuk Dipelajari!
-          </button>
-        </div>
-      </section>
+    <main className="flex items-center justify-center bg-[#fff7d7cc]">
+      <div className="w-full h-full flex gap-6 md:gap-8 flex-col px-[20px] py-[28px] md:px-[64px] md:py-[64px]">
+        <HomeHero />
+        <HomeContent />
+      </div>
     </main>
   );
 };
 
 export default HomeMain;
-
-//todo finish for desktop and please focus
